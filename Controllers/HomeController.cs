9756ae523fd578a63a5743e9ce87f5c75f5ef84e
Riyanka Gandhi <W0851269@myscc.ca>
2024-06-13 14:14:36 -0400

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace InkCraft.Controllers
             return View();
         }
 
+        public IActionResult Product()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
