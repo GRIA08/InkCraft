13577b2df278af293875e0c0b0f4dca31e46d9be
Riyanka Gandhi <W0851269@myscc.ca>
2024-06-13 13:57:34 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InkCraft.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
